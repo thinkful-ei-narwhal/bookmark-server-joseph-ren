ee3418eb7ad94c51e5c3c3df75cf21c308d0bff5
Joseph Rivera <josephr98@gmail.com>
2020-05-01 12:44:20 -0400

    added GET ':id' route

--- src/routes/bookmarks-router.js
+++ src/routes/bookmarks-router.js
@@ -1,20 +1,27 @@
 'use strict';
 const express = require('express');
 const bookmarksRouter = express.Router();
-const store = require('../store');
+const { bookmarks } = require('../store');
 const uuid = require('uuid/v4');
+
 
 bookmarksRouter
   .route('/')
   .get((req, res) => {
-    res.json(store);
+    res.json(bookmarks);
   })
   .post((req, res) => {});
 
 bookmarksRouter
   .route('/:id')
   .get((req, res) => {
-    res.send('ID working');
+    const { id } = req.params;
+    const selectedBookmark = bookmarks.filter(bookmark => bookmark.id == id)
+    if (selectedBookmark.length === 0) {
+      return res.status(404).json({ error: "Bookmark not found!" });
+    } else {
+      return res.status(200).json(selectedBookmark);
+    }
   })
   .delete((req, res) => {});
 
