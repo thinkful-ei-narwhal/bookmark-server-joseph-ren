95b4e89592d65c539e288de78e4608bdbed2bec4
Ren F <renfinn89@gmail.com>
2020-05-01 11:43:35 -0700

    finished

--- src/routes/bookmarks-router.js
+++ src/routes/bookmarks-router.js
@@ -5,7 +5,6 @@ const { bookmarks } = require('../store');
 const bodyParser = express.json();
 const uuid = require('uuid/v4');
 
-
 bookmarksRouter
   .route('/')
   .get((req, res) => {
@@ -14,32 +13,43 @@ bookmarksRouter
   .post(bodyParser, (req, res) => {
     const { title, url, description, rating } = req.body;
     if (!title || !url || !description || !rating) {
-      res.status(400).json({ error: "You need to include all fields" });
-    } 
-    if(!url.startsWith('https://')) {
-      res.status(400).json({ error: "You need to inculde valid 'https' protocal" })
+      res.status(400).json({ error: 'You need to include all fields' });
+    }
+    if (!url.startsWith('https://')) {
+      res
+        .status(400)
+        .json({ error: "You need to inculde valid 'https' protocal" });
     }
     bookmarks.push({
       id: uuid(),
       title,
       url,
       description,
-      rating: Number(rating)
-    })
-    res.json(bookmarks)
+      rating: Number(rating),
+    });
+    res.json(bookmarks);
   });
 
 bookmarksRouter
   .route('/:id')
   .get((req, res) => {
     const { id } = req.params;
-    const selectedBookmark = bookmarks.filter(bookmark => bookmark.id == id)
+    const selectedBookmark = bookmarks.filter((bookmark) => bookmark.id == id);
     if (selectedBookmark.length === 0) {
-      return res.status(404).json({ error: "Bookmark not found!" });
+      return res.status(404).json({ error: 'Bookmark not found!' });
     } else {
       return res.status(200).json(selectedBookmark);
     }
   })
-  .delete((req, res) => {});
+  .delete((req, res) => {
+    const { id } = req.params;
+    const findId = bookmarks.findIndex((i) => i.id == id);
+    if (findId === -1) {
+      return res.status(400).json({ error: 'No Bookmark found' });
+    } else {
+      bookmarks.splice(findId, 1);
+      return res.json(bookmarks);
+    }
+  });
 
 module.exports = bookmarksRouter;
