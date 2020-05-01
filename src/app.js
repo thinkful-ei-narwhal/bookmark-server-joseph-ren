0e6c557fbf82546037b22ff5281b00484658f71d
Joseph Rivera <josephr98@gmail.com>
2020-05-01 11:32:52 -0400

    configured base

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const helmet = require('helmet')
 const app = express()
 const { NODE_ENV } = require('./config')
 
+const bookmarksRouter = require('./routes/bookmarks-router')
+
+
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
@@ -15,11 +18,8 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+app.use('/bookmarks', bookmarksRouter);
 
-app.get('/', (req, res) => {
-  throw new Error('forced error')
-  res.send("Hello, world!")
-})
 
 app.use(function errorHandling(err, req, res, next) {
   let response;
---
+++ src/routes/bookmarks-router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bookmarksRouter = express.Router();
+
+bookmarksRouter
+  .route('/')
+  .get((req, res) => {
+    res.send('Routing working from')
+  })
+  .post((req, res) => {
+
+  })
+
+bookmarksRouter
+  .route('/:id')
+  .get((req, res) => {
+
+  })
+  .delete((req, res) => {
+    
+  })
+
+module.exports = bookmarksRouter;
